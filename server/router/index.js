8f184249f3861e0880e816a349931661db51cadc
themms <maximusnewera@mail.ru>
2023-07-08 23:56:02 +0600

    modified:   server/router/index.js

--- server/router/index.js
+++ server/router/index.js
@@ -1,6 +1,5 @@
 const Router = require('express').Router;
 const router = new Router();
-const taskModel = require('../models/task-model');
 const taskService = require('../services/task-service');
 
 router.get('/task', taskService.addTask);
